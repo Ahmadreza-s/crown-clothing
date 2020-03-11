cc19d4e3758978da8cadda6d3db4254caee66fe8
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-12 00:20:31 +0330

    added directory reducer, change directory class to functional component

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,62 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Directory.scss';
-
 import MenuItem from '../MenuItem/MenuItem';
+import {useSelector} from 'react-redux';
 
-class Directory extends Component {
-    state = {
-        sections: [
+const Directory = () => {
+    const sections = useSelector(state => state.directory.sections);
+    return (
+        <div className="directory-menu">
             {
-                title   : 'hats',
-                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                id      : 1,
-                linkUrl : 'shop/hats'
-            },
-            {
-                title   : 'jackets',
-                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id      : 2,
-                linkUrl : 'shop/jackets'
-            },
-            {
-                title   : 'sneakers',
-                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id      : 3,
-                linkUrl : 'shop/sneakers'
-            },
-            {
-                title   : 'women',
-                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                size    : 'large',
-                id      : 4,
-                linkUrl : 'shop/women'
-            },
-            {
-                title   : 'men',
-                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                size    : 'large',
-                id      : 5,
-                linkUrl : 'shop/men'
+                sections.map(section => (
+                    <MenuItem
+                        key={section.id}
+                        title={section.title}
+                        image={section.imageUrl}
+                        link={section.linkUrl}
+                        size={section.size}/>
+                ))
             }
-        ]
-    };
-
-    render() {
-        return (
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(section => (
-                        <MenuItem
-                            key={section.id}
-                            title={section.title}
-                            image={section.imageUrl}
-                            link={section.linkUrl}
-                            size={section.size}/>
-                    ))
-                }
-            </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Directory;
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,42 @@
+const initalState = {
+    sections: [
+        {
+            title   : 'hats',
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            id      : 1,
+            linkUrl : 'shop/hats'
+        },
+        {
+            title   : 'jackets',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            id      : 2,
+            linkUrl : 'shop/jackets'
+        },
+        {
+            title   : 'sneakers',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            id      : 3,
+            linkUrl : 'shop/sneakers'
+        },
+        {
+            title   : 'women',
+            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            size    : 'large',
+            id      : 4,
+            linkUrl : 'shop/women'
+        },
+        {
+            title   : 'men',
+            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            size    : 'large',
+            id      : 5,
+            linkUrl : 'shop/men'
+        }
+    ]
+};
+export default (state = initalState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
