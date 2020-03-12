fb8916f5b6794b4625ccdc7b2c4bbc93b99c7edb
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-12 20:59:39 +0330

    fix sneakers route

--- src/redux/shop/Shop.data.js
+++ src/redux/shop/Shop.data.js
@@ -1,5 +1,5 @@
 const SHOP_DATA = {
-    hats   : {
+    hats    : {
         id       : 1,
         title    : 'Hats',
         routeName: 'hats',
@@ -60,7 +60,7 @@ const SHOP_DATA = {
             }
         ]
     },
-    sneaker: {
+    sneakers: {
         id       : 2,
         title    : 'Sneakers',
         routeName: 'sneakers',
@@ -115,7 +115,7 @@ const SHOP_DATA = {
             }
         ]
     },
-    jackets: {
+    jackets : {
         id       : 3,
         title    : 'Jackets',
         routeName: 'jackets',
@@ -152,7 +152,7 @@ const SHOP_DATA = {
             }
         ]
     },
-    women  : {
+    women   : {
         id       : 4,
         title    : 'Womens',
         routeName: 'womens',
@@ -201,7 +201,7 @@ const SHOP_DATA = {
             }
         ]
     },
-    men    : {
+    men     : {
         id       : 5,
         title    : 'Mens',
         routeName: 'mens',
