bb1f876c174cb8528951d8e9d5681039f5a367b4
Ahmadreza Salehvand <ahmadreza.salehvand@gmail.com>
2020-03-23 16:46:08 +0430

    set some config for pwa

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './redux/store';
 
+import * as serviceWorker from './serviceWorker';
+
 const LoadingPersistor = () => <h2>Please wait ...</h2>;
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <PersistGate persistor={persistor} loading={<LoadingPersistor/>}>
-                <App/>
-            </PersistGate>
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById('root'));
+    (
+        <Provider store={store}>
+            <BrowserRouter>
+                <PersistGate persistor={persistor} loading={<LoadingPersistor/>}>
+                    <App/>
+                </PersistGate>
+            </BrowserRouter>
+        </Provider>
+    ),
+    document.getElementById('root'));
+
+serviceWorker.register();
